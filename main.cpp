c407dd7cb6004fa52c9c26d1f5e74c157028a215
Saleh Yahya <salehyahya10.20@gmail.com>
2026-02-17 01:17:42 -0600

    OrderID

--- main.cpp
+++ main.cpp
@@ -18,14 +18,34 @@
 #include <tuple>
 #include <format>
 
+enum class Ordertype {
+
+  GoodTillCancel,
+  FillAndKill,
+};
+
+
+enum class Side {
+  Buy,
+  Sell
+};
+
+
+using Price = std::int32_t;                          //int == 4 bytes //alias
+using Quantity = std::uint32_t;
+using OrderId = std::uint64_t;                      //int == 8 bytes
 
 
 
 
 
-using namespace std;
+
+
+
+
+
 
 int main() {
-...
+
   return 0;
 }
