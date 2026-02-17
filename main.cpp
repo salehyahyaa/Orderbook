a16d2779575db9894f733f6f482a64f53fd671c1
Saleh Yahya <salehyahya10.20@gmail.com>
2026-02-17 11:56:30 -0600

    Public/Priv API's

--- main.cpp
+++ main.cpp
@@ -36,6 +36,31 @@ using Quantity = std::uint32_t;
 using OrderId = std::uint64_t;                      //int == 8 bytes
 
 
+struct LevelInfo {                                 //used in public API's to get information about the state of the orderbook
+  Price price;
+  Quantity quantity;
+};
+
+using LevelInfos = std::vector<LevelInfo>; //LevelInfos == objects within the array Levelinfo {object1, object2}, each object within the array have INT Price/Quantity 
+
+class OrderbookLevelInfos {
+  
+  public:
+    OrderbookLevelInfos(const LevelInfos& bids, const LevelInfos& asks);
+      : bids_{ bids },
+        asks_{ asks }
+    { }
+
+    const LevelInfos& GetBids() const { return bids_;}       //public API
+    const LevelInfos& GetAsks() const { reuturn asks_;}      //Public API
+
+  private:
+
+
+
+
+};
+
 
 
 
