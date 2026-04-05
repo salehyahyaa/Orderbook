bf9deddad1d3873a5c105925cd8908ea2edb089c
Saleh Yahya <salehyahya10.20@gmail.com>
2026-04-05 11:01:35 -0500

    fixed OrderType.h

--- Ordertype.h
+++ Ordertype.h
@@ -1,16 +1,6 @@
 enum class OrderType {
-
   GoodTillCancel,
   FillAndKill,
+  GoodForDay,
+  Market
 };
-
-
-enum class Side {
-  Buy,
-  Sell
-};
-
-
-using Price = std::int32_t;                          //int == 4 bytes //alias
-using Quantity = std::uint32_t;
-using OrderId = std::uint64_t;                      //int == 8 bytes
