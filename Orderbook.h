83b354839d06afac1709dda70ec124fc94c434ee
Saleh Yahya <salehyahya10.20@gmail.com>
2026-04-09 14:43:00 -0500

    orderbook getters

--- Orderbook.h
+++ Orderbook.h
@@ -1,7 +1,7 @@
 #pramga once 
 
 #include <map> //dict in py
-#include <unorderd_map>
+#include <unordered_map>
 #include <thread>
 #include <condition_variable>
 #include <mutex>
@@ -12,27 +12,27 @@
 #include "OrderbookLevelInfos.h"
 #include "Trade.h"
 /*
-An orderbook is a catelog that maintains all active buy and sell orders for a financial instrument, organized by price and time priority
+An orderbook is a catelog that maintains all active buy and sell orders for a financial instrument, organized by price, time priority
 */
 
 
 class Orderbook {
-    private:                                              //when storing bids &&asks we'll use a map, bids sorted in ascneding order(best bids), asks sorted in desencding order(best ask), we will have O(1) easy access based on orderId
+    private:                                                        //when storing bids &&asks we'll use a map, bids sorted in ascneding order(best bids), asks sorted in desencding order(best ask), we will have O(1) easy access based on orderId
     
-      struct OrderEntry {                                 //represents an order and its location in an orderbook
-        OrderPointer order_{ nullptr };
-        OrderPointers::iterator location_;
+      struct OrderEntry {                                           //represents an order and its location in the orderbook
+        OrderPointer order_{ nullptr };                  
+        OrderPointers::iterator location_;                          //location holds the current value that iteratior's pointing at within OrderEntry
       };
 
-      std::map<Price, OrderPointers, std::greater<Price>> bids_;
-      std::map<Price, OrderPointers, std::less<Price>> asks_;
-      std::map<unordered_map, OrderEntry> orders_;
+      std::map<Price, OrderPointers, std::greater<Price>> bids_;    //auto sorts highest to lowest on bids
+      std::map<Price, OrderPointers, std::less<Price>> asks_;       //auto sorts lowest to highest on asks
+      std::unordered_map<OrderId,OrderEntry> orders_;               
 
 
       bool CanMatch(Side side, Price price) const {       //adding match method && canMatch method 
         if (side == Side::Buy) {                          //if incoming order is buy side
 
-          if (asks_,empty())
+          if (asks_.empty())
             return false;
           
           const auto& [bestAsk, ...] = *asks_.begin();
